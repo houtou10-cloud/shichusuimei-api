557095f91c033c21c56b2b79eab58c17409bc67e
houtou10-cloud <houtou10@gmail.com>
2026-08-09 13:46:32 +0900

    Update chart.py

--- engine/chart.py
+++ engine/chart.py
@@ -264,10 +264,6 @@ def calculate_chart(req) -> dict:
             chart_data
         )
     )
-    branch_clashes = find_branch_clashes(
-        chart_data
-
-    )
 
     branch_combinations = (
         find_branch_combinations(
